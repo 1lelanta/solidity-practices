8196eed126af4e25545a20e3b45f89eddb827bed
1lelanta <kekikoki4@gmail.com>
2026-01-30 09:39:28 +0300

    add mapping to convert name to the favorite number

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -10,15 +10,18 @@ contract SimpleStorage {
     }
 
     Person[] public listOfPeople;
+
+    mapping(string => uint256) public  nameToFavoriteNumber;
     function store(uint256 _favoriteNumber) public {
         myFavoriteNumber = _favoriteNumber;
     }
     function retrieve() public view  returns (uint256){
         return  myFavoriteNumber;
-    }
+    }  
 
     function addPerson(string memory _name, uint _favoriteNumber) public {
        
         listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
