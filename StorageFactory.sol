a4ff9aa83326922b0c6f74f73901276b2ab563da
1lelanta <kekikoki4@gmail.com>
2026-01-31 09:12:08 +0300

    import simple storage inside the simpleStorageFactory to deploy it using simple storageFactory

---
+++ StorageFactory.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import "./SimpleStorage.sol";
+
+contract StorageFactory{
+    SimpleStorage public simpleStorage;
+
+    function createSimpleStorage() public {
+        simpleStorage = new SimpleStorage();
+    }
+}
