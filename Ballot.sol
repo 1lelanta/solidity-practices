62f4b6f674ba55c779e4d9fb37c4907860a58fc8
1lelanta <kekikoki4@gmail.com>
2026-02-01 11:44:20 +0300

    add dynamic array of proposal to store each proposal on storage

--- Ballot.sol
+++ Ballot.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 contract Ballot{
 
     // shows what one voter looks like 
-    struct voter{
+    struct Voter{
         uint weight; // weight of the voting power 
         bool voted; // to avoid voting twice
         address delegate;// delegate address if any
@@ -12,10 +12,16 @@ contract Ballot{
     }
 
     // proposal structure
-    struct proposal{
+    struct Proposal{
         bytes32 name;  // small proposal name 
         uint voteCount; // vote counter 
     }
     // gives right to vote 
     address public  chairperson ;// address where contract is deployed
+
+    //map  every address to exactly  one voter record;
+    mapping (address=> Voter) public voters; // creates data base key ethereum address value voters
+
+    //create an array of proposal to store each propasal on storage
+    Proposal [] public  propasals; 
 }
