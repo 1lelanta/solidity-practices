29778b74868a52216741c22b16cfab28cc8d79ab
1lelanta <kekikoki4@gmail.com>
2026-02-01 10:08:03 +0300

    add fund me contract to get funds from user

---
+++ FundMe.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+// get funds from the user 
+// withdraw funds 
+// set minimum fundign value in USD
+
+contract FundMe{
+    function fund() public {
+
+    }
+
+    function withdraw() public {
+        
+    }
+}
