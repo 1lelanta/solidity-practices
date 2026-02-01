42cfa0f561d70028c2a0f69f68bf07d8f294d413
1lelanta <kekikoki4@gmail.com>
2026-02-01 11:31:56 +0300

    add structure of the proposal

--- Ballot.sol
+++ Ballot.sol
@@ -2,10 +2,18 @@
 pragma solidity ^0.8.18;
 
 contract Ballot{
+
+    // shows what one voter looks like 
     struct voter{
         uint weight; // weight of the voting power 
         bool voted; // to avoid voting twice
         address delegate;// delegate address if any
         uint vote; // index of the voted proposal
+    }
+
+    // proposal structure
+    struct proposal{
+        bytes32 name;  // small proposal name 
+        uint voteCount; // vote counter 
     }
 }
