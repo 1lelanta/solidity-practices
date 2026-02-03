db81b8e62320eb18b6bb092150111435c5a4ab39
1lelanta <kekikoki4@gmail.com>
2026-02-03 09:47:11 +0300

    cast price value to whole number

--- FundMe.sol
+++ FundMe.sol
@@ -18,11 +18,12 @@ contract FundMe{
     
 
     }
-        function getPrice() public {
+        function getPrice() public view returns(uint256) {
             // address  0x694AA1769357215DE4FAC081bf1f309aDC325306
             //ABI= Application Binary Interface. that tells outside world how to contact with that contract it is like a json 
             AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-             {, int256 price,,,}=  priceFeed.latestRoundData();
+             (,int256 price,,,)=  priceFeed.latestRoundData();
+             return uint256(price*1e10);
         }
         function getConversionRate() public view returns(uint256){
             return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
