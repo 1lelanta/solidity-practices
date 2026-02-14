e36cdbdbd222f3d82bd4fc86a98d3b2c0a946a16
1lelanta <kekikoki4@gmail.com>
2026-02-14 09:43:48 +0300

    create a functions that add two number dynamically and open door remotely

---
+++ Contracts/functions.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract learnFunctions{
+    function remoteControlOpen(bool opened) public returns(bool){
+
+    }
+
+    function addNumber(uint a, uint b) public view returns (uint){
+        return a + b;
+
+    }
+
+}
