3a47c2bb5def4790f067902b6172435b281bd097
1lelanta <kekikoki4@gmail.com>
2026-02-17 10:36:59 +0300

    create loop that iterate over list to check if it is multiples of given number

--- Contracts/loop.sol
+++ Contracts/loop.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.8.0;
 
 contract loopContract{
 
+    uint[] public numbers = [1,2,3,4,5,6,7,8,9];
+
+    function checkMultiple(uint _number) public view  returns(bool){
+
+        for(uint i = 0; i< numbers.length; i++){
+            checkMultiples(numbers[i], _number);
+
+    }
+
 
     function checkMultiples(uint _num, uint _nums) public view returns(bool){
 
